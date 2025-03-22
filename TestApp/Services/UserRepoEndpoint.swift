805b2cb6024988489a1666a79fde59f6a32f0419
Ivona Ilic <ivonailic@gmail.com>
2025-03-22 10:39:57 +0100

    Add UserRepoEndpoint for needed API calls

---
+++ TestApp/Services/UserRepoEndpoint.swift
@@ -0,0 +1,45 @@
+//
+//  UserRepoEndpoint.swift
+//  TestApp
+//
+//  Created by Ivona Ilic on 3/22/25.
+//
+
+import Foundation
+import Alamofire
+
+enum UserRepoEndpoint: ApiEndpoint {
+    
+    
+    case repos(user: String)
+    case repoDetails(repoName: String, user: String)
+    case tags(repoName: String, user: String)
+    
+    
+    
+    var path: String {
+        switch self {
+        case .repos(let user):
+            return "/users/\(user)/repos"
+        case .repoDetails(let repoName, let user):
+            return "/users/\(user)/\(repoName)"
+        case .tags(let repoName, let user):
+            return "/users/\(user)/\(repoName)/tags"
+        }
+    }
+    
+    var method: Alamofire.HTTPMethod {
+        switch self {
+        case .repos, .repoDetails, .tags:
+            return .get
+        }
+    }
+    
+    var isMultipart: Bool {
+        return false
+    }
+    
+    var parameters: Alamofire.Parameters? {
+        return nil
+    }
+}
